bd8ef75ddccdaecc832b9c61361ffea55f16afb0
arwgrvs <goal080813hgtrgna2@cbtis217.edu.mx>
2024-10-15 08:18:46 -0600

    formula_explain

--- formula.cpp
+++ formula.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 int main()
 {
+    //El programa consiste en un calculo de x1 y x2 donde se lea a, b y c para poder calcularlo, usando la formula general :D
     double a, b, c, x1=0, x2=0;
-    //Valores de a, b, y c
     cout<<"Asignar valores a a, b y c";
     cin>>a>>b>>c;
-    //Si son 0 todos los datos marcar error
     if(a==0&&b==0&&c==0)
     {
         cout<<"No se puede calcular :v\n";
         cin>>a>>b>>c;
     }
-    //Valor de x1
     x1+=(-1*b)+sqrt((b*b)-4*(a*c))/(2*a);
     cout<<"x1 es igual a: \n";
     printf("%f\n", x1);
-    //Valor de x2
     x2+=(-1*b)-sqrt((b*b)-4*(a*c))/(2*a);
     cout<<"x2 es igual a: \n";
     printf("%f\n", x2);
